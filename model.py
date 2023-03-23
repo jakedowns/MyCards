24515a1267917e0e4abbd9fdf2d83f694d10b29a
jakedowns <jdowns@wright20.com>
2023-03-22 18:32:07 -0700

    working on things

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from mycards.db import db
+from datetime import datetime
 
 class Deck(db.Model):
     __tablename__ = 'decks'
@@ -10,6 +11,33 @@ class Deck(db.Model):
     numRanks = db.Column(db.Integer, nullable=False)
     bgImage = db.Column(db.String(255), nullable=True)
     type = db.Column(db.String(80), nullable=False)
+    orientation = db.Column(db.String(80), nullable=True) # portrait or landscape
+    width = db.Column(db.Integer, nullable=True)
+    height = db.Column(db.Integer, nullable=True)
+    cards = db.relationship('Card', backref='deck', lazy=True)
+
+    # init named parameters:
+    # uid=data['uid'],
+    #     name=data['name'],
+    #     numCards=data['numCards'],
+    #     numSuits=data['numSuits'],
+    #     numRanks=data['numRanks'],
+    #     bgImage=data['bgImage'],
+    #     type=data['type'],
+    #     orientation=data.get('orientation'),
+    #     width=data.get('width'),
+    #     height=data.get('height'),
+    def __init__(self, name=None, uid=None, numCards=None, numSuits=None, numRanks=None, bgImage=None, type=None, orientation=None, width=None, height=None):
+        self.name = name
+        self.uid = uid
+        self.bgImage = bgImage
+        self.numCards = numCards
+        self.numSuits = numSuits
+        self.numRanks = numRanks
+        self.type = type
+        self.orientation = orientation if orientation is not None else "portrait"  # Default to portrait if not provided
+        self.width = width if width is not None else 0.0635  # Default to standard playing card width in meters if not provided
+        self.height = height if height is not None else 0.0889  # Default to standard playing card height in meters if not provided
 
     def __repr__(self):
         return f'<Deck {self.name}>'
@@ -23,5 +51,202 @@ class Deck(db.Model):
             'numSuits': self.numSuits,
             'numRanks': self.numRanks,
             'bgImage': self.bgImage,
-            'type': self.type
+            'type': self.type,
+            'cards': [card.to_dict() for card in self.cards]
+        }
+
+# Standard suit ranks mapping from int to values
+def get_name_of_suit(suit):
+    if suit is None:
+        return "Invalid suit"
+    suit = int(suit)
+    # Return the suit name based on the integer value provided
+    if suit == 0:
+        return "spades"
+    elif suit == 1:
+        return "hearts"
+    elif suit == 2:
+        return "diamonds"
+    elif suit == 3:
+        return "clubs"
+    else:
+        return "Invalid suit"
+
+# Method to convert rank index int to a string (A, 2 ... 10, J, Q, K)
+def rank_index_to_string(rank_index):
+    if rank_index is None:
+        return 'Invalid rank'
+
+    rank_index = int(rank_index)
+
+    if rank_index == 0:
+        return "A"
+    elif 1 <= rank_index <= 9:
+        return str(rank_index + 1)
+    elif rank_index == 10:
+        return "J"
+    elif rank_index == 11:
+        return "Q"
+    elif rank_index == 12:
+        return "K"
+    else:
+        return "Invalid rank"
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+    id = db.Column(db.Integer, primary_key=True)
+    deck_id = db.Column(db.Integer, db.ForeignKey('decks.id'), nullable=False)
+    rank = db.Column(db.String(80), nullable=True)
+    suit = db.Column(db.String(80), nullable=True)
+    orientation = db.Column(db.String(80), nullable=True)
+    width = db.Column(db.Integer, nullable=True)
+    height = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, deck_id, rank=None, suit=None, orientation=None, width=None, height=None):
+        self.deck_id = deck_id
+        self.rank = rank
+        self.suit = suit
+        self.orientation = orientation if orientation is not None else "portrait"
+        # Set default width and height if not provided
+        self.width = width if width is not None else 0.0635  # Standard playing card width in meters
+        self.height = height if height is not None else 0.0889  # Standard playing card height in meters
+
+    def __repr__(self):
+        suit_name = get_name_of_suit(self.suit)
+        rank_string = rank_index_to_string(self.rank)
+        return f'<Card {rank_string} of {suit_name}>'
+
+    def to_dict(self):
+        suit_name = get_name_of_suit(self.suit)
+        rank_string = rank_index_to_string(self.rank)
+        return {
+            'id': self.id,
+            'deck_id': self.deck_id,
+            'rank': self.rank,
+            'suit': self.suit,
+            'rank_string': rank_string,
+            'suit_name': suit_name,
+            'card_text': self.card_text.to_dict() if self.card_text else None,
+            'orientation': self.orientation,
+            'width': self.width,
+            'height': self.height
+        }
+
+class CardText(db.Model):
+    __tablename__ = 'card_texts'
+    id = db.Column(db.Integer, primary_key=True)
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.id'), nullable=False)
+    deck_id = db.Column(db.Integer, db.ForeignKey('decks.id'), nullable=False)
+    front_text = db.Column(db.Text, nullable=True)
+    rear_text = db.Column(db.Text, nullable=True)
+
+    card = db.relationship('Card', backref=db.backref('card_text', uselist=False), lazy=True)
+
+    def __repr__(self):
+        return f'<CardText {self.id}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'card_id': self.card_id,
+            'deck_id': self.deck_id,
+            'front_text': self.front_text,
+            'rear_text': self.rear_text
+        }
+
+# GameType model
+class GameType(db.Model):
+    __tablename__ = 'game_types'
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+
+# Playfield model
+# class Playfield(db.Model):
+#     __tablename__ = 'playfields'
+#     id = db.Column(db.Integer, primary_key=True)
+#     uid = db.Column(db.String, nullable=False)
+#     name = db.Column(db.String, nullable=False)
+#     game_id = db.Column(db.Integer, db.ForeignKey('game_types.id'), nullable=False)
+
+# MoveSet model
+class MoveSet(db.Model):
+    __tablename__ = 'movesets'
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('game_types.id'), nullable=False)
+
+# TODO: Implement PlayfieldArea model
+# class PlayfieldArea(db.Model):
+#     pass
+
+# TODO: Implement PlayfieldSpot model
+# class PlayfieldSpot(db.Model):
+#     pass
+
+# TODO: Implement GameMoveType model
+# class GameMoveType(db.Model):
+#     pass
+
+# TODO: Implement PlayerGameMove model
+# class PlayerGameMove(db.Model):
+#     pass
+
+# Implement GameInstance model
+class GameInstance(db.Model):
+    __tablename__ = 'game_instances'
+    id = db.Column(db.Integer, primary_key=True)
+    game_table_id = db.Column(db.Integer, db.ForeignKey('game_tables.id'), nullable=False)
+    game_type_id = db.Column(db.Integer, db.ForeignKey('game_types.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    deleted_at = db.Column(db.DateTime, nullable=True)
+
+# Implement PlayerGame pivot record
+class PlayerGame(db.Model):
+    __tablename__ = 'player_games'
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'), nullable=False)
+    game_table_id = db.Column(db.Integer, db.ForeignKey('game_tables.id'), nullable=False)
+    game_instance_id = db.Column(db.Integer, db.ForeignKey('game_instances.id'), nullable=False)
+
+    player = db.relationship('Player', backref='player_games', lazy=True)
+    game_table = db.relationship('GameTable', backref='player_games', lazy=True)
+    game_instance = db.relationship('GameInstance', backref='player_games', lazy=True)
+
+# Implement GameTable model
+class GameTable(db.Model):
+    __tablename__ = 'game_tables'
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    gametype_id = db.Column(db.Integer, db.ForeignKey('game_types.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    deleted_at = db.Column(db.DateTime, nullable=True)
+
+class Player(db.Model):
+    __tablename__ = 'players'
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    verified = db.Column(db.Boolean, nullable=False, default=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_logged_in_at = db.Column(db.DateTime, nullable=True)
+    deleted_at = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f'<Player {self.name}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'uid': self.uid,
+            'name': self.name,
+            'email': self.email,
+            'verified': self.verified,
+            'created_at': self.created_at,
+            'last_logged_in_at': self.last_logged_in_at,
+            'deleted_at': self.deleted_at
         }
--- routes.py
+++ routes.py
@@ -44,16 +44,71 @@ def create_deck():
     data = request.get_json()
 
     new_deck = Deck(
+        uid=data['uid'],
         name=data['name'],
         numCards=data['numCards'],
         numSuits=data['numSuits'],
         numRanks=data['numRanks'],
         bgImage=data['bgImage'],
-        type=data['type']
+        type=data['type'],
+        orientation=data.get('orientation'),
+        width=data.get('width'),
+        height=data.get('height'),
     )
 
-    db.session.add(new_deck)
-    db.session.commit()
+    # Check if a deck with the given uid already exists
+    existing_deck = Deck.query.filter_by(uid=data['uid']).first()
+
+    num_cards_before_update = existing_deck.numCards if existing_deck else -1
+    
+    if existing_deck:
+        # Update the existing deck with the new data
+        existing_deck.name = data['name']
+        existing_deck.numCards = data['numCards']
+        existing_deck.numSuits = data['numSuits']
+        existing_deck.numRanks = data['numRanks']
+        existing_deck.bgImage = data['bgImage']
+        existing_deck.type = data['type']
+        existing_deck.orientation = data.get('orientation')
+        existing_deck.width = data.get('width')
+        existing_deck.height = data.get('height')
+
+        new_deck.id = existing_deck.id
+
+        # Commit the changes to the database
+        db.session.commit()
+    else:
+        # Add the new deck to the session
+        db.session.add(new_deck)
+        db.session.commit()
+
+    # Create Card models for the number of suits and ranks
+    from mycards.model import Card
+
+    should_create_cards = False
+    if not existing_deck:
+        should_create_cards = True
+    elif num_cards_before_update != data['numCards']:
+        should_create_cards = True
+
+    if should_create_cards:
+        # Delete all cards for the deck
+        Card.query.filter_by(deck_id=new_deck.id).delete()
+        # Commit
+        db.session.commit()
+        
+        if data['numSuits'] <= 1 and data['numRanks'] <= 1:
+            for _ in range(data['numCards']):
+                new_card = Card(deck_id=new_deck.id, orientation=new_deck.orientation, width=new_deck.width, height=new_deck.height)
+                db.session.add(new_card)
+        else:
+            for suit in range(data['numSuits']):
+                for rank in range(data['numRanks']):
+                    new_card = Card(deck_id=new_deck.id, suit=suit, rank=rank, orientation=new_deck.orientation, width=new_deck.width, height=new_deck.height)
+                    db.session.add(new_card)
+
+        # Commit the cards to the database
+        db.session.commit()
 
     return jsonify({'message': 'Deck created successfully', 'deck_id': new_deck.id})
 
@@ -67,3 +122,87 @@ def get_decks():
 
     # Return the JSON array of decks
     return jsonify(decks_json)
+
+@app.route('/decks/<uid>', methods=['DELETE'])
+def delete_deck(uid):
+    # Find the deck with the given uid
+    deck = Deck.query.filter_by(uid=uid).first()
+
+    # If the deck is not found, return an error message
+    if not deck:
+        return jsonify({'message': 'Deck not found'}), 404
+
+    # Delete related CardText models
+    from mycards.model import CardText
+    CardText.query.filter_by(deck_id=deck.id).delete()
+
+    # Delete related card records
+    from mycards.model import Card
+    cards = Card.query.filter_by(deck_id=deck.id).all()
+    for card in cards:
+        db.session.delete(card)
+
+    # Delete the deck
+    db.session.delete(deck)
+    db.session.commit()
+
+    # Return a success message
+    return jsonify({'message': 'Deck deleted successfully'})
+
+@app.route('/card', methods=['POST'])
+def upsert_card():
+    from mycards.model import Card, CardText
+    data = request.get_json()
+
+    # strip the data.card_text object off the data into a separate variable
+    card_text = data['card_text']
+    data.pop('card_text', None)
+
+    # Create a new Card model
+    new_card = Card(
+        deck_id=data['deck_id'],
+        suit=data.get('suit'),
+        rank=data.get('rank'),
+        orientation=data.get('orientation'),
+        width=data.get('width'),
+        height=data.get('height')
+    )
+
+    # Add the new card to the session
+    db.session.add(new_card)
+    db.session.commit()
+
+    # Check if card_text is provided in the request data
+    if card_text:
+        # Create a new CardText model
+        new_card_text = CardText(
+            deck_id=data['deck_id'],
+            card_id=new_card.id, 
+            front_text=card_text['front_text'], 
+            rear_text=card_text['rear_text'])
+
+        # Add the new card text to the session
+        db.session.add(new_card_text)
+        db.session.commit()
+
+    return jsonify({'message': 'Card created successfully', 'card_id': new_card.id})
+
+@app.route('/cards/<uid>', methods=['DELETE'])
+def delete_card(uid):
+    # Find the card with the given uid
+    from mycards.model import Card, CardText
+    card = Card.query.filter_by(uid=uid).first()
+
+    # If the card is not found, return an error message
+    if not card:
+        return jsonify({'message': 'Card not found'}), 404
+
+    # Delete related CardText models
+    CardText.query.filter_by(card_id=card.id).delete()
+
+    # Delete the card
+    db.session.delete(card)
+    db.session.commit()
+
+    # Return a success message
+    return jsonify({'message': 'Card deleted successfully'})
